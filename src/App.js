aec2110ef6e40fdaebacac455d5aaae5240e3eba
Mohammed sarfaraz <mohammedsarfaraz18@gmail.com>
2023-06-08 19:22:10 +0530

    settimeout

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ function App() {
   const [obj, setobj] = useState({
     lift1: {
       direction: null,
-      currentPosition: 1,
+      currentPosition: 4,
       buffer: [],
     },
     lift2: {
       direction: null,
-      currentPosition: 2,
+      currentPosition: 1,
       buffer: [],
     },
   });
@@ -68,8 +68,7 @@ function App() {
   const elevStylesl2 = {
     bottom: `${floorl2 * 20 - 20}vh`,
   };
-
-  useEffect(() => {
+  setTimeout(() => {
     if (obj.lift1.direction === null && obj.lift2.direction === null) {
       if (upReq.length != 0) {
         const lift1diff = obj.lift1.currentPosition - upReq[0];
@@ -79,25 +78,41 @@ function App() {
           if (obj.lift1.currentPosition > upReq[0]) {
             const motion = { ...obj };
             motion.lift1.direction = "down";
+            if (upReq[0] != motion.lift1.currentPosition) {
+              motion.lift1.currentPosition -= 1;
+            }
+
             setobj(motion);
           } else if (obj.lift1.currentPosition < upReq[0]) {
             const motion = { ...obj };
             motion.lift1.direction = "up";
+            if (upReq[0] != motion.lift1.currentPosition) {
+              motion.lift1.currentPosition += 1;
+            }
             setobj(motion);
           }
         } else {
           if (obj.lift2.currentPosition > upReq[0]) {
             const motion = { ...obj };
             motion.lift2.direction = "down";
+            if (upReq[0] != motion.lift2.currentPosition) {
+              motion.lift2.currentPosition -= 1;
+            }
             setobj(motion);
           } else if (obj.lift2.currentPosition < upReq[0]) {
             const motion = { ...obj };
             motion.lift2.direction = "up";
+            if (upReq[0] != motion.lift2.currentPosition) {
+              motion.lift2.currentPosition += 1;
+              motion.lift2.buffer = motion.lift2.buffer.filter((ids) => {
+                return ids != upReq[0];
+              });
+            }
             setobj(motion);
           }
         }
       }
-
+      // after lift in motion
       if (downReq.length != 0) {
         const lift1diff = obj.lift1.currentPosition - downReq[0];
         const lift2diff = obj.lift2.currentPosition - downReq[0];
@@ -106,10 +121,16 @@ function App() {
           if (obj.lift1.currentPosition > downReq[0]) {
             const motion = { ...obj };
             motion.lift1.direction = "down";
+            if (upReq[0] != motion.lift1.currentPosition) {
+              motion.lift1.currentPosition -= 1;
+            }
             setobj(motion);
           } else if (obj.lift1.currentPosition < downReq[0]) {
             const motion = { ...obj };
             motion.lift1.direction = "up";
+            if (upReq[0] != motion.lift1.currentPosition) {
+              motion.lift1.currentPosition += 1;
+            }
             setobj(motion);
           }
           //assign lifts based nearest position
@@ -118,26 +139,21 @@ function App() {
           if (obj.lift2.currentPosition > downReq[0]) {
             const motion = { ...obj };
             motion.lift2.direction = "down";
+            if (upReq[0] != motion.lift2.currentPosition) {
+              motion.lift2.currentPosition -= 1;
+            }
             setobj(motion);
           } else if (obj.lift2.currentPosition < downReq[0]) {
             const motion = { ...obj };
             motion.lift2.direction = "up";
+            if (upReq[0] != motion.lift2.currentPosition) {
+              motion.lift2.currentPosition += 1;
+            }
             setobj(motion);
           }
         }
       }
     }
-  }, [upReq, downReq]);
-
-  const setNewDownReq = (value) => {
-    const downReq = setDownReq(value);
-  };
-
-  useEffect(() => {
-    // second in motion;
-    //while in upward motion follow reqPosition >= curPosition will be chosen
-
-    //while in down motion follow reqPosition <= curPosition will be chosen
     if (obj.lift1.direction === null && obj.lift2.direction !== null) {
       //ditect directionof lift 2
       const curDir = obj.lift2.direction;
@@ -179,12 +195,132 @@ function App() {
         setobj(clone);
         const clone2 = [...downReq];
         clone2.filter((ids) => !myDownReq.includes(ids));
-        setNewDownReq(clone2);
+        setDownReq(clone2);
       }
     } else if (obj.lift2.direction === null && obj.lift1.direction !== null) {
     } else {
     }
-  }, [upReq, downReq]);
+  }, 1000);
+
+  // useEffect(() => {
+  //   if (obj.lift1.direction === null && obj.lift2.direction === null) {
+  //     if (upReq.length != 0) {
+  //       const lift1diff = obj.lift1.currentPosition - upReq[0];
+  //       const lift2diff = obj.lift2.currentPosition - upReq[0];
+
+  //       if (Math.abs(lift1diff) <= Math.abs(lift2diff)) {
+  //         if (obj.lift1.currentPosition > upReq[0]) {
+  //           const motion = { ...obj };
+  //           motion.lift1.direction = "down";
+  //           setobj(motion);
+  //         } else if (obj.lift1.currentPosition < upReq[0]) {
+  //           const motion = { ...obj };
+  //           motion.lift1.direction = "up";
+  //           setobj(motion);
+  //         }
+  //       } else {
+  //         if (obj.lift2.currentPosition > upReq[0]) {
+  //           const motion = { ...obj };
+  //           motion.lift2.direction = "down";
+  //           setobj(motion);
+  //         } else if (obj.lift2.currentPosition < upReq[0]) {
+  //           const motion = { ...obj };
+  //           motion.lift2.direction = "up";
+  //           setobj(motion);
+  //         }
+  //       }
+  //     }
+
+  //     if (downReq.length != 0) {
+  //       const lift1diff = obj.lift1.currentPosition - downReq[0];
+  //       const lift2diff = obj.lift2.currentPosition - downReq[0];
+
+  //       if (Math.abs(lift1diff) <= Math.abs(lift2diff)) {
+  //         if (obj.lift1.currentPosition > downReq[0]) {
+  //           const motion = { ...obj };
+  //           motion.lift1.direction = "down";
+  //           setobj(motion);
+  //         } else if (obj.lift1.currentPosition < downReq[0]) {
+  //           const motion = { ...obj };
+  //           motion.lift1.direction = "up";
+  //           setobj(motion);
+  //         }
+  //         //assign lifts based nearest position
+  //         //both are at same floor send lift1
+  //       } else {
+  //         if (obj.lift2.currentPosition > downReq[0]) {
+  //           const motion = { ...obj };
+  //           motion.lift2.direction = "down";
+  //           setobj(motion);
+  //         } else if (obj.lift2.currentPosition < downReq[0]) {
+  //           const motion = { ...obj };
+  //           motion.lift2.direction = "up";
+  //           setobj(motion);
+  //         }
+  //       }
+  //     }
+  //   }
+  // }, [upReq, downReq]);
+
+  // const setNewDownReq = (value) => {
+  //   setDownReq(value);
+  // };
+  // const setNewUpReq = (value) => {
+  //   setUpReq(value);
+  // };
+
+  // useEffect(() => {
+  //   // second in motion;
+  //   //while in upward motion follow reqPosition >= curPosition will be chosen
+
+  //   //while in down motion follow reqPosition <= curPosition will be chosen
+  //   if (obj.lift1.direction === null && obj.lift2.direction !== null) {
+  //     //ditect directionof lift 2
+  //     const curDir = obj.lift2.direction;
+  //     console.log(upReq);
+  //     if (curDir === "up") {
+  //       const myUpReq = upReq.filter((ids) => ids >= obj.lift2.currentPosition);
+  //       console.log(upReq, "upreq");
+  //       const myDownReq = downReq.filter(
+  //         (ids) => ids >= obj.lift2.currentPosition
+  //       );
+  //       const temp = [...myDownReq, ...myUpReq];
+
+  //       //appen the accepted clients  to buffer and pop it from
+  //       // pop it from global up req arra
+  //       //change the lift position
+  //       // uncheck the button on based on direction of lift
+  //       console.log("upreq", upReq, " down2req", downReq, "my temp", temp);
+
+  //       const clone = { ...obj };
+  //       clone.lift2.buffer.push(...myUpReq);
+  //       setobj(clone);
+  //       const clone2 = [...upReq];
+  //       clone2.filter((ids) => !myUpReq.includes(ids));
+  //       setNewUpReq(clone2);
+  //     } else if (curDir === "down") {
+  //       const myUpReq = upReq.filter((ids) => ids >= obj.lift2.currentPosition);
+  //       const myDownReq = downReq.filter(
+  //         (ids) => ids >= obj.lift2.currentPosition
+  //       );
+  //       const temp = [...myDownReq, ...myUpReq];
+  //       //appen the accepted clients  to buffer and pop it from
+  //       // pop it from global up req arra
+  //       //change the lift position
+  //       // uncheck the button on based on direction of lift
+  //       console.log("upreq", upReq, " down2req", downReq, "my temp", temp);
+
+  //       const clone = { ...obj };
+  //       clone.lift2.buffer.push(...myDownReq);
+  //       setobj(clone);
+  //       const clone2 = [...downReq];
+  //       clone2.filter((ids) => !myDownReq.includes(ids));
+  //       setNewDownReq(clone2);
+  //     }
+  //   } else if (obj.lift2.direction === null && obj.lift1.direction !== null) {
+  //   } else {
+  //   }
+  // }, [upReq, downReq]);
 
   return (
     <div className="App">
